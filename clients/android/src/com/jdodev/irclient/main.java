bdd31c7c4b44a0efd000495701692d4a5cd9d996
jan-dorgeville <jdorgeville@innobec.com>
2015-07-08 21:04:59 -0400

    update android

--- clients/android/src/com/jdodev/irclient/main.java
+++ clients/android/src/com/jdodev/irclient/main.java
@@ -1,7 +1,11 @@
 package com.jdodev.irclient;
 
+import com.jdodev.ircodec.IRBuffer;
+import com.jdodev.ircodec.IRcodec;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -35,7 +39,10 @@ public class main extends Activity implements TCPClient.Listener {
 			public void onClick(View v) {
 				if (mTCP != null) {
 					int[] data =  new int[] { 0xFF, 0xF7, 0xC0, 0x3F};
-					mTCP.send(IRcodec.int2byte(data));
+					String bits = IRcodec.Bytes2String(data, 32);
+					Log.d(TAG, "send "+bits.length()+" bits!");
+					byte[] ir = IRcodec.NecEncode(bits);					
+					mTCP.send(ir);
 				}
 			}
         });
@@ -84,6 +91,8 @@ public class main extends Activity implements TCPClient.Listener {
 	@Override
 	public void OnThreadStart() {
 		mTxtInformation.setText("Wait...");
+		IRcodec.Unkn0DecodeReset();
+		IRcodec.NecDecodeReset();
 	}
 
 	@Override
@@ -95,7 +104,24 @@ public class main extends Activity implements TCPClient.Listener {
 	
 	@Override
 	public void OnDataReceived(byte[] data, int sz) {
-		mTxtInformation.setText("data received");
+		IRBuffer ir = new IRBuffer();
+		ir.buffer = data;
+		ir.len = sz;		
+		do {
+			if (IRcodec.Unkn0Decode(ir)==1)
+			{				
+				String decodedbits = IRcodec.Unkn0DecodedBuff();
+				Log.d(TAG, "received "+decodedbits.length()+" bits!");
+				int[] decoded = IRcodec.String2Bytes(decodedbits);				
+				if(decodedbits.length()==39 && decoded[0]==0xAB) {
+					if (decoded[0]==0xAB) {
+						float weight = (float)(decoded[2]<<8 | decoded[3])/10;
+						mTxtInformation.setText(""+weight+"Kg");
+					}
+				}				
+				IRcodec.Unkn0DecodeReset();
+			}
+		} while(ir.available());		
 	}
 
 	
