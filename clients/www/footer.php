bc7bcbc795065a3f24f45856d0b538c4e2cdc173
jan <jdorgeville@gmail.com>
2015-07-17 19:34:22 -0400

    remove www client

--- clients/www/footer.php
+++
@@ -1,6 +0,0 @@
-<?PHP 
-// WARNING: NO TEXT SHOULD BE WRITE HERE (EXCEPT DIE ERRORS)
-// BE CAREFULL THAT THEY ALSO NO TEXT (EVEN SPACE) OUTSIDE PHP BALISES
-// -> cause this file is used under download.php
-  mysql_close($db); 
-?>
